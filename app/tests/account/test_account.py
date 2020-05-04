ad3320a6fdbb4dd36f308972750f0c6d45aaaf20
Teja <phankanmp@gmail.com>
2020-05-03 19:29:33 -0700

    add additional tests for account app

--- app/tests/account/test_account.py
+++ app/tests/account/test_account.py
@@ -2,6 +2,9 @@ from django.contrib.auth import get_user_model
 from rest_framework.reverse import reverse
 from rest_framework import status
 
+from expense.models import Expense
+from account.serializers import UserSerializer
+
 import base64
 import json
 
@@ -43,6 +46,28 @@ class TestAuthentication:
         assert response.data["email"] == user.email
         assert response.data["first_name"] == user.first_name
         assert response.data["last_name"] == user.last_name
+        assert user.get_full_name() == f"{user.first_name} {user.last_name}"
+        assert str(user) == f"{user.get_full_name()} <{user.email}>"
+        assert user.has_perm("Yes") is True
+        assert user.has_module_perms(Expense) is True
+
+    def test_user_can_sign_up_mismatch_password_serializer(self, client):
+        """
+        Test for user serializer with mistmatch password
+        """
+        invalid_serializer_data = {
+            "email": "user@example.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "password1": PASSWORD,
+            "password2": "mismatchPassword",
+        }
+
+        serializer = UserSerializer(data=invalid_serializer_data)
+        assert not serializer.is_valid()
+
+        with pytest.raises(Exception):
+            UserSerializer.validate(invalid_serializer_data)
 
     def test_user_can_log_in(self, client, create_user):
         """
@@ -64,3 +89,70 @@ class TestAuthentication:
         assert payload_data["email"] == user.email
         assert payload_data["first_name"] == user.first_name
         assert payload_data["last_name"] == user.last_name
+
+    def test_create_superuser(self, client):
+        """
+        Test for create superuser
+        """
+        superuser = get_user_model().objects.create_superuser(
+            email="superuser@example.com",
+            first_name="SuperTest",
+            last_name="User",
+            password=PASSWORD,
+        )
+
+        assert superuser.email == "superuser@example.com"
+        assert superuser.first_name == "SuperTest"
+        assert superuser.last_name == "User"
+        assert superuser.is_staff is True
+        assert superuser.is_superuser is True
+        assert superuser.is_active is True
+
+    def test_create_superuser_invalid(self, client):
+        """
+        Test for superuser is_staff and is_superuser are True
+        """
+        with pytest.raises(Exception):
+            get_user_model().objects.create_superuser(
+                email="superuser@example.com",
+                first_name="SuperTest",
+                last_name="User",
+                password=PASSWORD,
+                is_staff=False,
+                is_superuser=True,
+            )
+
+        with pytest.raises(Exception):
+            get_user_model().objects.create_superuser(
+                email="superuser@example.com",
+                first_name="SuperTest",
+                last_name="User",
+                password=PASSWORD,
+                is_staff=True,
+                is_superuser=False,
+            )
+
+    def test_create_user_invalid(self, client):
+        """
+        Test for superuser has email, first, and last name
+        """
+        with pytest.raises(Exception):
+            get_user_model().objects.create_user(
+                email="", first_name="Test", last_name="User", password=PASSWORD
+            )
+
+        with pytest.raises(Exception):
+            get_user_model().objects.create_user(
+                email="user@example.com",
+                first_name="",
+                last_name="User",
+                password=PASSWORD,
+            )
+
+        with pytest.raises(Exception):
+            get_user_model().objects.create_user(
+                email="user@example.com",
+                first_name="Test",
+                last_name="",
+                password=PASSWORD,
+            )
